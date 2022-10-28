9f34ddab4e2f7b81d31fe6036a1d41b21081bdec
Akash123-coder <82027340+Akash123-coder@users.noreply.github.com>
2022-10-28 16:09:47 +0530

    remove bug and footer

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// let weather={
-// apiKey :"03d32da4e4b248e559a276b75c50bc4f",
-// fetchWeather: function(city){
-// fetch(
-//     "https://api.openweathermap.org/data/2.5/weather?q= "+ city +"&appid="+ this.apiKey,
-// )
-// .then((Response)=>Response.json())
-// .then((data)=>console.log(data));
-// },
+
 let weather = {
   apiKey: "03d32da4e4b248e559a276b75c50bc4f",
   fetchWeather: function (city) {
@@ -32,7 +24,6 @@ displayWeather: function(data)
 //  const{icon,description}=data.main;
  const{temp,humidity}=data.main;
  const {speed}=data.wind;
-//  console.log(name,icon,description,temp,humidity,speed)
 
  document.querySelector(".city").innerText="Weather in" +name;
  document.querySelector(".icon").src =
@@ -63,4 +54,3 @@ document
   });
 
 weather.fetchWeather("Denver");
-// weather.displayWeather(data);
